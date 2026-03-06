59d95e3742a46732f677be53d3ccbaee5b6c2734
Kumar Aditya <kumaraditya93134@gmail.com>
2026-03-07 00:12:54 +0530

    fixing upload

--- src/lib/r2.ts
+++ src/lib/r2.ts
@@ -2,7 +2,8 @@ import { S3Client } from "@aws-sdk/client-s3";
 
 export const r2 = new S3Client({
   region: "auto",
-  endpoint: `https://${process.env.CLOUDFLARE_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+  endpoint: process.env.R2_ENDPOINT, // 👈 change this
+  forcePathStyle: true,
   credentials: {
     accessKeyId: process.env.CLOUDFLARE_ACCESS_KEY_ID!,
     secretAccessKey: process.env.CLOUDFLARE_SECRET_ACCESS_KEY!,
